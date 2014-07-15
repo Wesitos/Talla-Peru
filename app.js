b12b1f58cb01cbeb715e96ce78d4a13d68017222
hanshavin <hansevangelista@gmail.com>
2014-07-15 07:27:29 -0500

    simple backend del sitio

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+	swig = require('swig')
+var app = express()
+
+app.engine('html', swig.renderFile)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/app/views')
+
+app.use(express.static('./public'))
+
+app.get('/', function(req, res){
+	//res.send('Hello World');
+	res.render('index')
+})
+
+var server = app.listen(3000)
