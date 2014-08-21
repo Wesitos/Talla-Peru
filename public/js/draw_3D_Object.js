d4206be1b69152f76ff2ed48da991872d9fb3b40
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2014-08-21 00:58:18 -0500

    Whitespaces and stuff

--- public/js/draw_3D_Object.js
+++ public/js/draw_3D_Object.js
@@ -7,29 +7,29 @@
 
 /*$(container).bind('move', function (e) {
 
-    object.rotation.y += e.deltaX * 0.005;
-    object.rotation.x += e.deltaY * 0.005;
+  object.rotation.y += e.deltaX * 0.005;
+  object.rotation.x += e.deltaY * 0.005;
 
-    render(scene, camera, renderer);
-});*/
+  render(scene, camera, renderer);
+  });*/
 
 var elements;
 var global;
 
-function draw_3D_Object (container) { 
-    
-    var mesa;
-    var renderer,camera,scene;    
+function draw_3D_Object (container) {
 
-    
+    var mesa;
+    var renderer,camera,scene;
+
+
     elements = initial_setup(container);
-    
+
     renderer = elements.renderer;
-    camera = elements.camera;    
-    scene = elements.scene;    
-    mesa = elements.mesa;      
+    camera = elements.camera;
+    scene = elements.scene;
+    mesa = elements.mesa;
     console.log("hoka")
-    
+
     console.log("bye")
 
 
@@ -38,31 +38,31 @@ function draw_3D_Object (container) {
 function girar(container){
     console.log("girar");
     renderer = elements.renderer;
-    camera = elements.camera;    
-    scene = elements.scene;    
-    
-    
-    
+    camera = elements.camera;
+    scene = elements.scene;
+
+
+
     $(container).bind('move', function (e) {
         object.rotation.y += e.deltaX * 0.005;
         object.rotation.x += e.deltaY * 0.005;
         render(scene, camera, renderer);
     })
-    
+
 }
 
 function initial_setup (container){
-//Configuramos la camara y el renderer inicial
+    //Configuramos la camara y el renderer inicial
     var loaded;
-    
+
     var width = window.innerWidth/2;
-    var height = window.innerWidth/2;    
-    
+    var height = window.innerWidth/2;
+
     // Creamos el objeto a "renderear" y lo agregamos al DOM
-    var renderer = new THREE.WebGLRenderer( { alpha: true } );        
-    renderer.setSize(1000,1000)    
+    var renderer = new THREE.WebGLRenderer( { alpha: true } );
+    renderer.setSize(1000,1000)
     container.appendChild(renderer.domElement);
-    
+
     //Creamos la camara que va mostrar la escena
     var camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 1000);
     camera.position.z = 200;
@@ -70,37 +70,37 @@ function initial_setup (container){
     camera.position.y = 20;
     //Creamos la escena que va mostar el objeto
     var scene = new THREE.Scene();
-    
+
     scene.add(camera);
-    
-    
+
+
     /*Agregamos sth */
     var manager = new THREE.LoadingManager();
-    var loader = new THREE.OBJLoader(manager);        
-    
+    var loader = new THREE.OBJLoader(manager);
+
     global = loader;
-    
-    manager.onProgress = function (item,loaded,total){        
-        
-        console.log( item, loaded, total );        
-        
-    }    
-    
-    
+
+    manager.onProgress = function (item,loaded,total){
+
+        console.log( item, loaded, total );
+
+    }
+
+
     loader.load('./obj/mesa-split.obj', function (object) {
         //loaded = object;
-        scene.add(object);        
+        scene.add(object);
         console.log("cargue!");
-        render(scene, camera, renderer);            
+        render(scene, camera, renderer);
     })
-    
-    
+
+
     $(container).bind('move', function (e) {
         object.rotation.y += e.deltaX * 0.005;
         object.rotation.x += e.deltaY * 0.005;
         render(scene, camera, renderer);
     });
-    
+
     return {
         scene : scene,
         mesa : loaded,
@@ -110,11 +110,6 @@ function initial_setup (container){
 }
 
 function render (scene, camera, renderer) {
-//Render con los nuevos parametros de renderer    
+    //Render con los nuevos parametros de renderer
     renderer.render(scene, camera);
-}
-
-
-function girar (){    
-    
 }
