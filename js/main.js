615c1f9d668259aa2abc7685788723da5dddad81
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2014-09-05 21:48:44 -0500

    Eliminados comentarios

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
-
 $(document).ready(function () {
-
-	//console.log('hola!')
-	/*
-	$('#slider').rhinoslider({
-		effect: 'fade',
-		showTime: 5000,
-		autoPlay: true
-	});*/
-	/*
-    window.myFlux = $('#slider').flux({
-        autoplay: false,
-        pagination: true
-    });*/
-
-	//menu()
-	/*
-	modal()
-	*/
-	//var container = document.getElementById('piece-furniture-3D')
-	var container = document.getElementById('mesa')	
-	draw_3D_Object(container)
-	
+        var container = document.getElementById('mesa');
+        draw_3D_Object(container);
 })
