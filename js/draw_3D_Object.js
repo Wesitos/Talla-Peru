f6e8c0bbccafe92a1ed2397fd0e9f594d2006792
Fredy Morales <saiberz@hotmail.com>
2014-10-08 21:37:15 -0500

    Prueba librero mesa en el nuevo html

--- js/draw_3D_Object.js
+++ js/draw_3D_Object.js
@@ -1,22 +1,55 @@
 var elements;
 var global;
-var showed = false;
+var showed = null ;
+
+var modelo = [
+    {
+        nombre: "Cama",
+        id: "cama",
+        loaded: false,
+        showed: false,
+        path: "/obj/librero/librero(cama).obj"
+    },
+
+    {
+        nombre: "Librero",
+        id: "librero",
+        loaded: false,
+        showed: false,
+        path: "/obj/librero/librero(cerrado).obj"
+    },
+
+    {
+        nombre: "Mesa",
+        id: "mesa",
+        loaded: false,
+        showed: false,
+        path: "/obj/librero/librero(mesa).obj"
+    },
+    {
+        nombre: "Escritorio Repisa",
+        id: "escritorio2",
+        loaded: false,
+        showed: false,
+        path: false
+    }    
+];
+
 
 function draw_3D_Object (container) {
-    elements = initial_setup(container);
-    select_type();
+    elements = initial_setup(container);    
 }
 
 function scale(container, object){
     var nomx = 1,
-    nomy = 2,
-    nomz = 0.75;
+        nomy = 2,
+        nomz = 0.75;
     var maxx = 1.8,
-    maxy = 2.5,
-    maxz = 0.85;
+        maxy = 2.5,
+        maxz = 0.85;
     var minx = 0.8,
-    miny = 1.6,
-    minz = 0.6;
+        miny = 1.6,
+        minz = 0.6;
 
     $("#controlx").bind("change",function(){
         var value = $(this).val()/nomx;
@@ -74,19 +107,6 @@ function scale(container, object){
 
 }
 
-function toogleObj(obj1, obj2){
-    $("#transform").bind("click", function(){
-        if (obj1.visible){
-            showObject(obj1, false);
-            showObject(obj2, true);
-        }
-        else{
-            showObject(obj1, true);
-            showObject(obj2, false);
-        }
-        render();
-    });
-}
 
 function girar(container, object){
     //Event handler para girar la mesa
@@ -137,30 +157,16 @@ function initial_setup (container){
         console.log(item, loaded, total);
     });
 
-    var manager_mesa = new THREE.LoadingManager(function onLoad(){
-        elements.loaded.mesa = true;
+    var manager_object = new THREE.LoadingManager(function onLoad(){
+        elements.loaded = true;
+        var firstid  = document.getElementsByName("estado")[0]["value"];
+        showed = firstid;
+        showObject(elements[firstid],true);
+        
     }, function onProgress (item, loaded, total){
         console.log(item, loaded, total);
     });
-
-    var manager_librero = new THREE.LoadingManager(function onLoad(){
-        elements.loaded.librero = true;
-    }, function onProgress (item, loaded, total){
-        console.log(item, loaded, total);
-    });
-
-    var manager_escritorio = new THREE.LoadingManager(function onLoad(){
-        elements.loaded.escritorio = true;
-    }, function onProgress (item, loaded, total){
-        console.log(item, loaded, total);
-    });
-
-    var manager_sofa = new THREE.LoadingManager(function onLoad(){
-        elements.loaded.sofa = true;
-    }, function onProgress (item, loaded, total){
-        console.log(item, loaded, total);
-    });
-
+  
     //--------------------------------
     // Cargamos la textura
     var texture_1 = new THREE.Texture();
@@ -190,136 +196,36 @@ function initial_setup (container){
     } );
 
     // Cargamos el modelo
-    var loader_mesa = new THREE.OBJLoader(manager_mesa);
-    var loader_librero = new THREE.OBJLoader(manager_librero);
-    var loader_escritorio = new THREE.OBJLoader(manager_escritorio);
-    var loader_sofa = new THREE.OBJLoader(manager_sofa);
+    var loader_object = new THREE.OBJLoader(manager_object);  
 
-    loader_mesa.load('./obj/mesa/mesa-comedor.obj', function (obj) {
-        obj.traverse( function ( child ) {
-            if ( child instanceof THREE.Mesh ) {
-                child.material.map = texture_1;
-            }});
-        obj.name = "mesa_comedor";
-        showObject(obj, true);
-        showed = "mesa_comedor";
-        elements.mesa_comedor = obj;
-        scene.add(obj);
-        girar(container, obj);
-        scale(container,obj);
-    });
+    modelo.forEach (load_model);
+    var id = "cama";
 
-    loader_mesa.load('./obj/mesa/mesa-centro.obj', function (obj) {
-        obj.traverse( function ( child ) {
-            if ( child instanceof THREE.Mesh ) {
-                child.material.map = texture_1;
-            }});
-        obj.name = "mesa_centro";
-        showObject(obj, false);
-        elements.mesa_centro = obj;
-        scene.add(obj);
+    function load_model (value, index, arr){
+        if(value.path){
+            loader_object.load(value.path, function (obj) {
+                obj.traverse( function ( child ) {
+                    if ( child instanceof THREE.Mesh ) {
+                        child.material.map = texture_1;
+                    }});
+                obj.name = value.nombre;
+                obj.showed = value.showed;
+                showObject(obj, false);            
+                console.log("+++++" + value.id);
+                elements[value.id] = obj;
+                scene.add(obj);
+                girar(container, obj);
+                scale(container,obj);
+            });        
+        }
 
-        girar(container, obj);
-        scale(container,obj);
-    });
-
-    loader_librero.load('./obj/librero/librero(cama).obj', function (obj) {
-        obj.traverse( function ( child ) {
-            if ( child instanceof THREE.Mesh ) {
-                if(child.name == "colchon")
-                {child.material = colchon_material;}
-                else
-                {child.material.map = texture_1;}
-            }});
-        obj.name = "cama_librero";
-        showObject(obj, false);
-        elements.cama_librero = obj;
-        scene.add(obj);
-        girar(container, obj);
-    });
-
-    loader_librero.load('./obj/librero/librero(cerrado).obj', function (obj) {
-        obj.traverse( function ( child ) {
-            if ( child instanceof THREE.Mesh ) {
-                if(child.name == "colchon")
-                {child.material = colchon_material;}
-                else
-                {child.material.map = texture_1;}
-            }});
-        obj.name = "librero";
-        showObject(obj, false);
-        elements.librero = obj;
-        scene.add(obj);
-        girar(container, obj);
-    });
-
-    // Cama-escritorio
-    loader_escritorio.load('./obj/escritorio/camascritorio(cama).obj', function (obj) {
-        obj.traverse( function ( child ) {
-            if ( child instanceof THREE.Mesh ) {
-                if(child.name == "colchon")
-                {child.material = colchon_material;}
-                else
-                {child.material.map = texture_1;}
-            }});
-        obj.name = "cama";
-        showObject(obj, false);
-        elements.cama = obj;
-        scene.add(obj);
-        girar(container, obj);
-    });
-
-    loader_escritorio.load('./obj/escritorio/camascritorio(escritorio).obj', function (obj) {
-        obj.traverse( function ( child ) {
-            if ( child instanceof THREE.Mesh ) {
-                if(child.name == "colchon")
-                {child.material = colchon_material;}
-                else
-                {child.material.map = texture_1;}
-            }});
-        obj.name = "escritorio";
-        showObject(obj, false);
-        elements.escritorio = obj;
-        scene.add(obj);
-        girar(container, obj);
-    });
-
-    // Sofa-camarote
-    loader_sofa.load('./obj/sofa/sofamarote(camarote).obj', function (obj) {
-        obj.traverse( function ( child ) {
-            if ( child instanceof THREE.Mesh ) {
-                if(child.name == "colchon")
-                {child.material = colchon_material;}
-                else
-                {child.material.map = texture_1;}
-            }});
-        obj.name = "camarote";
-        showObject(obj, false);
-        elements.camarote = obj;
-        scene.add(obj);
-        girar(container, obj);
-    });
-
-    loader_sofa.load('./obj/sofa/sofamarote(sofa).obj', function (obj) {
-        obj.traverse( function ( child ) {
-            if ( child instanceof THREE.Mesh ) {
-                if(child.name == "colchon")
-                {child.material = colchon_material;}
-                else
-                {child.material.map = texture_1;}
-            }});
-        obj.name = "sofa";
-        showObject(obj, false);
-        elements.sofa = obj;
-        scene.add(obj);
-        girar(container, obj);
-    });
-
+    } 
+  
     return {
         loaded: loaded,
         scene: scene,
         camera: camera,
-        renderer: renderer,
+        renderer: renderer
     };
 }
 
@@ -331,7 +237,8 @@ function render () {
     renderer.render(scene, camera);
 }
 
-function showObject(obj, show){
+function showObject(obj, show){    
+
     if (show){
         obj.visible = true;
         obj.traverse( function(child){
@@ -345,120 +252,25 @@ function showObject(obj, show){
             if ( child instanceof THREE.Mesh ) {
                 child.visible = false;
             }});
-    }
+    }        
+    render();    
 }
 
-function select_type (){
-    $("#menu_muebles").val(1);
-    radio = document.getElementById("opt1");
-    radio.checked = true;
 
-    $( "#menu_muebles" ).change(menu_callback);
-    menu_callback();
-    $("input[name=tipo]").click(function(){
-        val = this.id;
-        val = val[3]; // Modo 1 o 2 del mueble
-        grupo = $("#menu_muebles").val();
-        show_object_by_id(grupo,val);
-    });
+$("input[type=radio]").click(function(){
+    myval = this;
+    id = myval.value;
+    if(showed)
+        showObject(elements[showed],false);    
+    showObject(elements[id],true);     
+    showed = id;  
+});
 
 
-}
 
-function show_object_by_id(grupo,tipo){
-    grupo = parseInt(grupo)-1;
-    tipo = parseInt(tipo)-1;
-    id_object = [
-        ["mesa_comedor", "mesa_centro"],
-        ["librero", "cama_librero"],
-        ["escritorio", "cama"],
-        ["sofa", "camarote"],
-    ];
-    h = tipo*(-1)+1;
-    obj_show = id_object[grupo][tipo];
-    obj_hide = id_object[grupo][h];
-
-    // obj_show es el objeto que debe ser mostrado
-    if (showed)
-    {showObject(elements[showed],false);}
-    showObject(elements[obj_show],true);
-    showed = obj_show;
-
-    render();
+document.getElementsByName("estado")[0].checked = true
 
 
-}
 
-function menu_callback(){
-    clearTimeout(elements.timer);
-    var canvas_obj = document.getElementById("renderer_canvas");
-    var loading_obj = document.getElementById("loading");
-    var dim_div = document.getElementById("dim_div");
-    var menu_val = document.getElementById("menu_muebles").value;
-    var controles = document.getElementById("controles");
 
-    var name;
-    var val1;
-    var val2;
-    var show_dim_controls;
 
-    switch (menu_val){
-    case "1":
-        name = "mesa";
-        val1 = 'Mesa Comedor';
-        val2 = 'Mesa Centro';
-        show_dim_controls = true;
-        break;
-    case "2":
-        name = "librero";
-        val1 = 'Librero';
-        val2 = 'Cama';
-        show_dim_controls = false;
-        break;
-    case "3":
-        name = "escritorio";
-        val1 = 'Escritorio';
-        val2 = 'Cama';
-        show_dim_controls = false;
-        break;
-    case "4":
-        name = "sofa";
-        val1 = 'Sofa';
-        val2 = 'Camarote';
-        show_dim_controls = false;
-        break;
-    }
-    // Verificamos si los objetos estan cargados
-    var loadingScreenTimeout = function loadingScreenTimeout(){
-        if (elements.loaded[name] && elements.loaded.texture){
-            // Oculta el loading Screen
-            loading_obj.style.display = "none";
-            canvas_obj.style.display = "";
-
-            //Muestra los controles
-            controles.style.display = "";
-            if (show_dim_controls)
-            {dim_div.style.display = "";}
-            else
-            {dim_div.style.display = "none";}
-
-            //Muestra el modelo
-            show_object_by_id(menu_val,1);
-
-            //Cambia los radio button
-            radio = document.getElementById("opt1").checked = true;
-            $('label[for=opt1]').html(val1);
-            $('label[for=opt2]').html(val2);
-        }
-        else{
-            loading_obj.style.display = "";
-            canvas_obj.style.display = "none";
-            controles.style.display = "none";
-            dim_div.style.display = "none";
-            elements.timer = setTimeout(loadingScreenTimeout, 200);
-        }
-    };
-
-    loadingScreenTimeout();
-
-}
